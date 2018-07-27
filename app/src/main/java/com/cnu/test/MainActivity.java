d7ce2e94ab7adefcc794356c6f6d95e8446c58d5
srini <srinivasvadde@gmail.com>
2018-07-27 22:51:32 +0530

    adding test files

--- app/src/main/java/com/cnu/test/MainActivity.java
+++ app/src/main/java/com/cnu/test/MainActivity.java
@@ -1,52 +1,76 @@
 package com.cnu.test;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.widget.TextView;
+
+import com.onmo.wgames.sdk.IResponseHandler;
+import com.onmo.wgames.sdk.IWGameSession;
+import com.onmo.wgames.sdk.LibMainActivity;
+import com.onmo.wgames.sdk.OnmoWGSDK;
+import com.onmo.wgames.sdk.SDKException;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "MainActivity";
+    private TextView tv_method_1;
+    private TextView tv_method_2;
+    private TextView response_message;
+    private IWGameSession mWGSession ;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        tv_method_1 = findViewById(R.id.tv_method_1);
+        tv_method_2 = findViewById(R.id.tv_method_2);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        response_message = findViewById(R.id.response_message);
+        // getting session object to access the SDK methods
+        mWGSession = OnmoWGSDK.newInitializer(MainActivity.this)
+                .build();
+
+        tv_method_1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+
+
+                if(mWGSession!=null)
+                {
+                    mWGSession.getStoreUser(new IResponseHandler<String>() {
+                        @Override
+                        public void handleResponse(String aUserId) {
+                            response_message.setText("User id:"+aUserId);
+                        }
+
+                        @Override
+                        public void handleException(SDKException exception) {
+                            response_message.setText("error::"+exception.getMessage());
+                        }
+                    });
+                }
+            }
+        });
+
+        tv_method_2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+
+                try {
+                    Intent intent = new Intent(MainActivity.this, LibMainActivity.class);
+                    startActivity(intent);
+                }
+                catch (Exception ex)
+                {
+                    Log.d(TAG, "Exception --->"+ex.getMessage());
+                }
             }
         });
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
